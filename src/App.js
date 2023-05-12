2cdecf51dff12aa948ef2254339532ea55c90102
Pavel <kaynov@group-ib.com>
2023-05-12 16:22:49 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Push into main branch initiates deploy.
         </p>
         <a
           className="App-link"
