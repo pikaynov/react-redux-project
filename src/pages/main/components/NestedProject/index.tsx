98af09929aeb402710ed6f089493f03b6e0902af
DieKeyIgo <kaynovd@gmail.com>
2023-06-02 19:01:42 +0300

    added styles.css in nested project

--- src/pages/main/components/NestedProject/index.tsx
+++ src/pages/main/components/NestedProject/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import css from './styles.css';
 
 export const NestedProject = () => {
-    return <div>Nested Project</div>;
+    return (<body className={css.box}>
+        <div>Nested Project <br />
+            <img id='Pashka-Stameska' src='https://i.ibb.co/4psXzmm/images.jpg' alt='avatar' />
+        </div>
+    </body>)
 }
+
+//<link rel="stylesheet" type='text/css' href="index.css" />
