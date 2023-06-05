09a3d2b4392050e443523187bc07b43e0da02945
DieKeyIgo <kaynovd@gmail.com>
2023-06-05 21:12:50 +0300

    creating project/nested branch

--- src/pages/main/components/NestedProject/index.tsx
+++ src/pages/main/components/NestedProject/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import css from './styles.css';
 
-export const NestedProject = () => {
-    return (<body className={css.box}>
-        <div>Nested Project <br />
-            <img id='Pashka-Stameska' title='Pashok in anger' src='https://i.ibb.co/4psXzmm/images.jpg' alt='avatar' />
-        </div>
-    </body>)
-}
+export const NestedProject = () => (<body className={css.box}>
+    <div>Nested Project <br />
+        <img id='Pashka-Stameska' title='Pashok in anger' src='https://i.ibb.co/4psXzmm/images.jpg' alt='avatar' />
+    </div>
+</body>)
