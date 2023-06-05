901f03a8752cb762cea0b9c85e5f825c513d881b
DieKeyIgo <kaynovd@gmail.com>
2023-06-05 16:45:30 +0300

    added

--- src/pages/main/components/NestedProject/index.tsx
+++ src/pages/main/components/NestedProject/index.tsx
@@ -4,9 +4,7 @@ import css from './styles.css';
 export const NestedProject = () => {
     return (<body className={css.box}>
         <div>Nested Project <br />
-            <img id='Pashka-Stameska' src='https://i.ibb.co/4psXzmm/images.jpg' alt='avatar' />
+            <img id='Pashka-Stameska' title='Pashok in anger' src='https://i.ibb.co/4psXzmm/images.jpg' alt='avatar' />
         </div>
     </body>)
 }
-
-//<link rel="stylesheet" type='text/css' href="index.css" />
