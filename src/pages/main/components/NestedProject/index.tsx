35b0dc36bcb9e613792e5e2617bc313cb58883a2
DieKeyIgo <kaynovd@gmail.com>
2023-06-05 19:16:17 +0300

    changed img to gigached

--- src/pages/main/components/NestedProject/index.tsx
+++ src/pages/main/components/NestedProject/index.tsx
@@ -4,7 +4,7 @@ import css from './styles.css';
 export const NestedProject = () => {
     return (<body className={css.box}>
         <div>Nested Project <br />
-            <img id='Pashka-Stameska' title='Pashok in anger' src='https://i.ibb.co/4psXzmm/images.jpg' alt='avatar' />
+            <img id='Pashka-Stameska' title='Pashok Gigachad' src='https://avatars.yandex.net/get-music-content/5502420/a1febdfe.a.24116597-1/400x400' alt='avatar' />
         </div>
     </body>)
 }
